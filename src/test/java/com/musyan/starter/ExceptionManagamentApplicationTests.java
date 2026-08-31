bd5961fb32796ac52388572769859d187704b70b
mustafayanmaz <mustafa.yanmaz.tr@gmail.com>
2026-08-31 20:47:02 +0300

    Add JUnit test with BeforeEach and AfterEach lifecycle methods

--- src/test/java/com/musyan/starter/ExceptionManagamentApplicationTests.java
+++ src/test/java/com/musyan/starter/ExceptionManagamentApplicationTests.java
@@ -1,13 +1,38 @@
 package com.musyan.starter;
 
+import com.musyan.dto.DtoEmployee;
+import com.musyan.service.IEmployeeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {ExceptionManagamentStarter.class})
 class ExceptionManagamentApplicationTests {
 
+
+	@Autowired
+	private IEmployeeService employeeService;
+
+	@BeforeEach
+	public void beforeEach(){
+		System.out.println("beforeEach çalıştı...");
+	}
+
 	@Test
-	void contextLoads() {
+	public void testFindEmployeeById(){
+
+		DtoEmployee employee = employeeService.findEmployeeById(28L);
+
+		if(employee != null){
+			System.out.println("İsim : "+ employee.getName());
+		}
+	}
+
+	@AfterEach
+	public void afterEach(){
+		System.out.println("AfterEach çalıştı...");
 	}
 
 }
